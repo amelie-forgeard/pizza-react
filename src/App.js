2698ef06f64114002fa186aee8380414b38e2df9
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-26 14:08:37 +0200

    ingredients ok with z index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 // import React, { useState, useEffect } from "react";
+import { useState } from "react";
+
 import Header from "./components/Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Customize from "./components/Customize";
 import Checkout from "./components/Checkout";
 
 export default function App() {
+    // on crée un state pour la gestion des ingredients:
+    // dans le state initial j'ai aucun ingrédient
+    const [ingredients, setIngredients] = useState({
+        basil: false,
+        cheese: false,
+        mushroom: false,
+        olive: false,
+        pineapple: false,
+        tomato: false,
+    });
     return (
         <div className="App">
             <Header />
@@ -12,8 +24,8 @@ export default function App() {
                 <Switch>
                     <Route exact path="/">
                         <Customize
-                        //   ingredients={ingredients}
-                        //   setIngredients={setIngredients}
+                            ingredients={ingredients}
+                            setIngredients={setIngredients}
                         />
                     </Route>
                     <Route path="/checkout">
